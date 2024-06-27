ff7087843c5ec647907f0664c52a12df399a9ef1
Igor <igorlis159@gmail.com>
2024-06-27 20:09:52 +0300

    Commit 1

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,32 +1,89 @@
-import java.util.ArrayList;
-import java.util.Objects;
-import java.util.Queue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.stream.IntStream;
 
-public class FizzBuzz implements Runnable {
-    private Integer n;
+public class FizzBuzz {
+    private final int n;
+    private int current = 1;
+    BlockingQueue<String> queue = new LinkedBlockingQueue<>();
 
-    FizzBuzz(Integer n) {
+    public FizzBuzz(int n) {
         this.n = n;
     }
 
-    @Override
-    public void run() {
-        System.out.println("");
-
-        for (int i = 0; i < n; i++) {
-
-                if ((i + 1) % 5 == 0 && (i + 1) % 3 == 0) {
-                    System.out.println("fizzbuzz");
-                } else if ((i + 1) % 5 == 0) {
-                    System.out.println("buzz");
-                } else if ((i + 1) % 3 == 0) {
-                    System.out.println("fizz");
-                } else {
-                    System.out.println(i + 1);
+    public synchronized void fizz() {
+        while (current <= n) {
+            if (current % 3 == 0 && current % 5 != 0) {
+                queue.add("fizz");
+                //System.out.println("fizz");
+                current++;
+                notifyAll();
+            } else {
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
                 }
 
+            }
         }
+    }
+
+    public synchronized void buzz() {
+        while (current <= n) {
+            if (current % 5 == 0 && current % 3 != 0) {
+                queue.add("buzz");
+//                System.out.println("buzz");
+                current++;
+                notifyAll();
+            } else {
+                try {
+                    wait();
+                } catch (InterruptedException e){
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
+    }
+
+    public synchronized void fizzbuzz() {
+        while (current <= n) {
+            if (current % 3 == 0 && current % 5 == 0) {
+                queue.add("fizzbuzz");
+//                System.out.println("fizzbuzz");
+                current++;
+                notifyAll();
+            } else {
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+
+    public synchronized void number() {
+        while (current <= n) {
+            synchronized (this) {
+                if (current % 3 != 0 && current % 5 != 0) {
+                    queue.add(String.valueOf(current));
+                    current++;
+                    notifyAll();
+                } else {
+                    try {
+                        wait();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        }
+
+        System.out.println(queue.toString());
+
     }
 
 
 }
+
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-//       Thread clock = new Thread(new Clock());
-//        clock.start();
-        Thread fizzBuzz = new Thread(new FizzBuzz(15));
-        fizzBuzz.start();
+       Thread clock = new Thread(new Clock());
+        clock.start();
 
+
+        FizzBuzz fizzBuzz = new FizzBuzz(15);
+        Thread threadA = new Thread(fizzBuzz::fizz);
+        Thread threadB = new Thread(fizzBuzz::buzz);
+        Thread threadC = new Thread(fizzBuzz::fizzbuzz);
+        Thread threadD = new Thread(fizzBuzz::number);
+
+        threadA.start();
+        threadB.start();
+        threadC.start();
+        threadD.start();
+
+        try {
+            threadA.join();
+            threadB.join();
+            threadC.join();
+            threadD.join();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
     }
 }
